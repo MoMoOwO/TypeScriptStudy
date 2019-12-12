484f6903b27e86917aa79498b1c5440d69aa1d4f
strangersj <strangersj@outlook.com>
2019-12-12 21:57:41 +0800

    TypeScript中的装饰器_方法装饰器、方法参数装饰器、装饰器的执行顺序

--- 018TypeScript中的装饰器_方法装饰器、方法参数装饰器、装饰器的执行顺序/index.ts
+++ 018TypeScript中的装饰器_方法装饰器、方法参数装饰器、装饰器的执行顺序/index.ts
@@ -57,4 +57,96 @@ console.log(http.apiUrl);
 http.run();
 console.log("--");
 http.getInfo(123, true, "111dd"); // 执行装饰器修改的方法
+console.log("----------");
+
+// 方法参数装饰器
+/**
+ * 方法参数装饰器：
+ * 方法参数装饰器表达式会在运行时当作函数被调用，可以使用参数装饰器为类的原型只能加一些元素数据，
+ * 传入以下三个参数：
+ * 第一个参数：对于静态成员来说是类的构造函数，对于实例成员是类的原型对象
+ * 第二个参数：方法的名称
+ * 第三个参数：参数在函数参数列表中的索引
+ */
+// 定义一个装饰器
+function paramsDecorator(params: any) {
+    return function (target: any, methodName: any, paramsIndex: number) {
+        console.log(params); // 调用装饰器时传递的参数
+        console.log(target);  // 对于静态成员来说是类的构造函数，对于实例成员是类的原型对象
+        console.log(methodName); // 方法的名称
+        console.log(paramsIndex); // 方法参数在参数列表中的索引
+
+        // 可以通过taget参数来扩展类的属性和方法
+        target.addAttr = params;
+    }
+}
+
+class Animal{
+    type: string | undefined;
+    constructor() {
+        
+    }
+    // 方法参数装饰器与其他装饰器类似，只需要在参数前面加上@装饰器的名称即可
+    eat(@paramsDecorator("扩展的属性") food: string):void {
+        console.log("吃" + food);
+    }
+}
+
+let an: any = new Animal();
+an.eat("食物");
+console.log(an.addAttr);
+console.log("-------------");
+
+// 类装饰器、属性装饰器、方法装饰器、方法参数装饰器的执行顺序：
+// 属性装饰器 > 方法装饰器 > 方法参数装饰器 > 类装饰器，多个同类型的装饰器从后往前执行
+// 类装饰器
+function logClass1(params: string) {
+    return function (target: any) {
+        console.log("类装饰器1");
+    }
+}
+function logClass2(params: string) {
+    return function (target: any) {
+        console.log("类装饰器2");
+    }
+}
+// 属性装饰器
+function logAttribute(params: string) {
+    return function (target: any, attrName: string) {
+        console.log("属性装饰器");
+    }
+}
+// 方法装饰器
+function logFunction(params: string) {
+    return function (target: any, methodName: string, desc: any) {
+        console.log("方法装饰器");
+    }
+}
+// 方法参数装饰器
+function logParams1() {
+    return function (target: any, methodName: string, paramsIndex: number) {
+        console.log("参数装饰器1");
+    }
+}
+function logParams2() {
+    return function (target: any, methodName: string, paramsIndex: number) {
+        console.log("参数装饰器2");
+    }
+}
+@logClass1("我是类装饰器1")
+@logClass2("我是类装饰器2")
+class Demo{
+    @logAttribute("我是属性装饰器")
+    demoAttr: string | undefined;
+    constructor() {
+        
+    }
+    @logFunction("我是方法装饰器呀")
+    getData() {
+        console.log("DemoFunction");
+    }
+    setData(@logParams1() attr1: any, @logParams2() attr2: any) {
+        
+    }
+}
 
--- 018TypeScript中的装饰器_方法装饰器、方法参数装饰器、装饰器的执行顺序/js/index.js
+++ 018TypeScript中的装饰器_方法装饰器、方法参数装饰器、装饰器的执行顺序/js/index.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 // TypeScript中的装饰器
 // 3.方法装饰器  它会被应用到方法的属性描述符上，可以用来监视、修改或者替换方法的定义
 /**
@@ -75,3 +78,99 @@ console.log(http.apiUrl);
 http.run();
 console.log("--");
 http.getInfo(123, true, "111dd"); // 执行装饰器修改的方法
+console.log("----------");
+// 方法参数装饰器
+/**
+ * 方法参数装饰器：
+ * 方法参数装饰器表达式会在运行时当作函数被调用，可以使用参数装饰器为类的原型只能加一些元素数据，
+ * 传入以下三个参数：
+ * 第一个参数：对于静态成员来说是类的构造函数，对于实例成员是类的原型对象
+ * 第二个参数：方法的名称
+ * 第三个参数：参数在函数参数列表中的索引
+ */
+// 定义一个装饰器
+function paramsDecorator(params) {
+    return function (target, methodName, paramsIndex) {
+        console.log(params); // 调用装饰器时传递的参数
+        console.log(target); // 对于静态成员来说是类的构造函数，对于实例成员是类的原型对象
+        console.log(methodName); // 方法的名称
+        console.log(paramsIndex); // 方法参数在参数列表中的索引
+        // 可以通过taget参数来扩展类的属性和方法
+        target.addAttr = params;
+    };
+}
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    // 方法参数装饰器与其他装饰器类似，只需要在参数前面加上@装饰器的名称即可
+    Animal.prototype.eat = function (food) {
+        console.log("吃" + food);
+    };
+    __decorate([
+        __param(0, paramsDecorator("扩展的属性"))
+    ], Animal.prototype, "eat", null);
+    return Animal;
+}());
+var an = new Animal();
+an.eat("食物");
+console.log(an.addAttr);
+console.log("-------------");
+// 类装饰器、属性装饰器、方法装饰器、方法参数装饰器的执行顺序：
+// 属性装饰器 > 方法装饰器 > 方法参数装饰器 > 类装饰器，多个同类型的装饰器从后往前执行
+// 类装饰器
+function logClass1(params) {
+    return function (target) {
+        console.log("类装饰器1");
+    };
+}
+function logClass2(params) {
+    return function (target) {
+        console.log("类装饰器2");
+    };
+}
+// 属性装饰器
+function logAttribute(params) {
+    return function (target, attrName) {
+        console.log("属性装饰器");
+    };
+}
+// 方法装饰器
+function logFunction(params) {
+    return function (target, methodName, desc) {
+        console.log("方法装饰器");
+    };
+}
+// 方法参数装饰器
+function logParams1() {
+    return function (target, methodName, paramsIndex) {
+        console.log("参数装饰器1");
+    };
+}
+function logParams2() {
+    return function (target, methodName, paramsIndex) {
+        console.log("参数装饰器2");
+    };
+}
+var Demo = /** @class */ (function () {
+    function Demo() {
+    }
+    Demo.prototype.getData = function () {
+        console.log("DemoFunction");
+    };
+    Demo.prototype.setData = function (attr1, attr2) {
+    };
+    __decorate([
+        logAttribute("我是属性装饰器")
+    ], Demo.prototype, "demoAttr", void 0);
+    __decorate([
+        logFunction("我是方法装饰器呀")
+    ], Demo.prototype, "getData", null);
+    __decorate([
+        __param(0, logParams1()), __param(1, logParams2())
+    ], Demo.prototype, "setData", null);
+    Demo = __decorate([
+        logClass1("我是类装饰器1"),
+        logClass2("我是类装饰器2")
+    ], Demo);
+    return Demo;
+}());
